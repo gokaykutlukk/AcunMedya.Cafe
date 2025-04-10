a64f1a8f8f01cf1d5e7b01f9c06b5f6168b22010
Gökay KUTLUK <gokaykutluk1@gmail.com>
2025-04-10 13:21:09 +0300

    09.04.25 /2

--- AcunMedya.Cafe/Controllers/ProductController.cs
+++ AcunMedya.Cafe/Controllers/ProductController.cs
@@ -59,6 +59,50 @@ namespace AcunMedya.Cafe.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id) 
+        {
+            var values = _context.Products.Find(id);
+            List<SelectListItem> valuesCategory = (from x in _context.Categories.ToList()
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryId.ToString()
+                                                   }).ToList();
+            ViewBag.v = valuesCategory;
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product model)
+        {
+            if (model.ImageFile != null)
+            {
+                // uygulamanın çalıştığı dizini alıyoruz
+                var currenDirectory = Directory.GetCurrentDirectory();
+
+                // resim dosyasının uzantısını alıyoruz jpg , png, jpeg
+                var extension = Path.GetExtension(model.ImageFile.FileName);
+                //benzersiz dosya oluşturuyoruz
+                var fileName = Guid.NewGuid().ToString();
+                // resim dosyasını kaydetmek için bir yol oluşturuyoruz
+                var saveLocation = Path.Combine(currenDirectory + "/wwwroot/images/", fileName + extension);
+                // resim dosyasını kaydediyoruz
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                model.ImageFile.CopyTo(stream);
+                model.ImageUrl = "/images/" + fileName + extension;
+            }
+
+            _context.Products.Update(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var values = _context.Products.Find(id);
+            _context.Products.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
     }
 }
